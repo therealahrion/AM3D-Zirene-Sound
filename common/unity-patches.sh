ef80f9e6a336f650f3e02bd1f95d076881c068dd
ahrion <Ahrionmgallegos@gmail.com>
2017-08-20 15:48:08 -0600

    add conditional statements instead of sed removing lines

--- common/unity-patches.sh
+++ common/unity-patches.sh
@@ -6,14 +6,9 @@
 #
 #
 # ^ DO NOT MODIFY ^
-for CFG in $CONFIG_FILE $HTC_CONFIG_FILE $OTHER_V_FILE $OFFLOAD_CONFIG $V_CONFIG_FILE; do
-  if [ -f $CFG ]; then
-    sed -i '/am3daudioenhancement {/,/}/d' $UNITYPATCH$CFG
-  fi
-done
 ui_print "    Patching existing audio_effects files..."
 for CFG in $CONFIG_FILE $HTC_CONFIG_FILE $OTHER_V_FILE $OFFLOAD_CONFIG $V_CONFIG_FILE; do
-  if [ -f $CFG ]; then
+  if [ -f $CFG ] && [ ! "$(cat $AMLPATH$CFG | grep ' am3daudioenhancement {')" ]; then
     sed -i 's/^effects {/effects {\n  am3daudioenhancement {\n    library am3daudioenhancement\n    uuid 6723dd80-f0b7-11e0-98a2-0002a5d5c51b\n  }/g' $UNITYPATCH$CFG
     sed -i 's/^libraries {/libraries {\n  am3daudioenhancement {\n    path \/system\/lib\/soundfx\/libam3daudioenhancement.so\n  }/g' $UNITYPATCH$CFG
   fi
