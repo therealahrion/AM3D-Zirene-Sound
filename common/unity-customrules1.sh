74f22dbe5e9d18c6aa5a8ad4117cef99fa11c064
Zackptg5 <zackptg5@comcast.net>
2017-10-02 23:24:54 -0400

    No need for custom logic

--- common/unity-customrules1.sh
+++ common/unity-customrules1.sh
@@ -5,9 +5,4 @@
 # You can create copies of this file and name is the same as this but with the next number after it (ex: unity-customrules2.sh)
 # See instructions for TIMEOFEXEC values, do not remove it
 # ^ DO NOT MODIFY ^
-TIMEOFEXEC=3
-$CP_PRFX $INSTALLER/custom/presets.xml $UNITY$SYS/etc/presets/presets.xml$CP_SFFX
-$CP_PRFX $INSTALLER/custom/TransducerEQ_XML/Foxconn/TransducerEQ.xml $UNITY$SYS/etc/presets/TransducerEQ_XML/Foxconn/TransducerEQ.xml$CP_SFFX
-$CP_PRFX $INSTALLER/custom/TransducerEQ_XML/Fugoo_Speaker/Fugoo_v1_TEQ_v1.xml $UNITY$SYS/etc/presets/TransducerEQ_XML/Fugoo_Speaker/Fugoo_v1_TEQ_v1.xml$CP_SFFX
-$CP_PRFX $INSTALLER/custom/TransducerEQ_XML/JBL_Flip/M810_BT_TEQ_v2.xml $UNITY$SYS/etc/presets/TransducerEQ_XML/JBL_Flip/M810_BT_TEQ_v2.xml$CP_SFFX
-$CP_PRFX $INSTALLER/custom/TransducerEQ_XML/JBL_Micro_Wireless/JBL_MICRO_WIRELESS_TEQ_v2.xml $UNITY$SYS/etc/presets/TransducerEQ_XML/JBL_Micro_Wireless/JBL_MICRO_WIRELESS_TEQ_v2.xml$CP_SFFX
+TIMEOFEXEC=
