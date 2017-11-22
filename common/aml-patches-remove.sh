f27fc01bcea256156a6cd0dfeaf1aea4f5e4d6d8
Zackptg5 <zackptg5@comcast.net>
2017-11-22 15:13:40 -0500

    Autodetect all conf/pol/mix files

--- common/aml-patches-remove.sh
+++ common/aml-patches-remove.sh
@@ -1,5 +1,3 @@
-for CFG in $CONFIG_FILE $HTC_CONFIG_FILE $OTHER_V_FILE $OFFLOAD_CONFIG $V_CONFIG_FILE; do
-  if [ -f $CFG ]; then
-	sed -i '/am3daudioenhancement {/,/}/d' $AMLPATH$CFG
-  fi
+for FILE in ${CFGS}; do
+  sed -i '/am3daudioenhancement {/,/}/d' $AMLPATH$FILE
 done
