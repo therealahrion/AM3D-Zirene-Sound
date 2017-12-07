c7bbba2c12b238d4a6ddb65b66b45651ac3f91b6
Zackptg5 <zackptg5@comcast.net>
2017-12-07 00:25:57 -0500

    Add xml cfg support, make xml separate from conf variables

--- common/aml-patches-remove.sh
+++ common/aml-patches-remove.sh
@@ -1,6 +1,9 @@
 for FILE in ${CFGS}; do
   sed -i '/am3daudioenhancement {/,/}/d' $AMLPATH$FILE
 done
+for FILE in ${CFGSXML}; do
+  sed -i '/am3daudioenhancement/d' $AMLPATH$FILE
+done
 # if [ ! -z $XML_PRFX ]; then
   # # Enter xmlstarlet logic here
 # fi
