d6a6fd5032d0810f199b5ceceeea22f1025f190e
Zackptg5 <zackptg5@comcast.net>
2017-12-11 08:14:30 -0500

    Use double quotes

--- common/aml-patches-remove.sh
+++ common/aml-patches-remove.sh
@@ -1,6 +1,6 @@
 for FILE in ${CFGS}; do
-  sed -i '/am3daudioenhancement {/,/}/d' $AMLPATH$FILE
+  sed -i "/am3daudioenhancement {/,/}/d" $AMLPATH$FILE
 done
 for FILE in ${CFGSXML}; do
-  sed -i '/<!--$MODID-->/d' $AMLPATH$FILE
+  sed -i "/<!--$MODID-->/d" $AMLPATH$FILE
 done
--- common/aml-patches.sh
+++ common/aml-patches.sh
@@ -1,8 +1,8 @@
 ui_print "    Patching existing audio_effects files..."
 for FILE in ${CFGS}; do
   if [ ! "$(grep "am3daudioenhancement" $AMLPATH$FILE)" ]; then
-    sed -i 's/^effects {/effects {\n  am3daudioenhancement {\n    library am3daudioenhancement\n    uuid 6723dd80-f0b7-11e0-98a2-0002a5d5c51b\n  }/g' $AMLPATH$FILE
-    sed -i 's/^libraries {/libraries {\n  am3daudioenhancement {\n    path $LIBPATCH\/lib\/soundfx\/libam3daudioenhancement.so\n  }/g' $AMLPATH$FILE
+    sed -i "s/^effects {/effects {\n  am3daudioenhancement {\n    library am3daudioenhancement\n    uuid 6723dd80-f0b7-11e0-98a2-0002a5d5c51b\n  }/g" $AMLPATH$FILE
+    sed -i "s/^libraries {/libraries {\n  am3daudioenhancement {\n    path $LIBPATCH\/lib\/soundfx\/libam3daudioenhancement.so\n  }/g" $AMLPATH$FILE
   fi
 done
 for FILE in ${CFGSXML}; do
