751c807e8d5040f2e28a48104f4d8acc5f02403c
Zackptg5 <zackptg5@comcast.net>
2018-02-05 19:12:43 -0500

    Revert "Just set audioserver to permissive"
    This reverts commit 42b96d03c6c499fea0728394b75d29c153ba2ebf.

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -1,9 +1,11 @@
 if [ "$SEINJECT" != "/sbin/sepolicy-inject" ]; then
-  $SEINJECT --live "permissive audioserver"
+  $SEINJECT --live "allow audioserver audioserver_tmpfs file { read write execute }"
+  $SEINJECT --live "allow audioserver system_file file execmod"
   $SEINJECT --live "allow mediaserver mediaserver_tmpfs file { read write execute }"
   $SEINJECT --live "allow mediaserver system_file file execmod"
 else
-  $SEINJECT -Z audioserver -l
+  $SEINJECT -s audioserver -t audioserver_tmpfs -c file -p read,write,execute -l
+  $SEINJECT -s audioserver -t system_file -c file -p execmod -l
   $SEINJECT -s mediaserver -t mediaserver_tmpfs -c file -p read,write,execute -l
   $SEINJECT -s mediaserver -t system_file -c file -p execmod -l
 fi
