3eac7b72d967d7c522cfefe0d07c3afb29d40eac
Zackptg5 <zackptg5@comcast.net>
2018-02-12 10:55:28 -0500

    Fix sepolicy patching

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -1,8 +1,5 @@
 if [ "$SEINJECT" != "/sbin/sepolicy-inject" ]; then
-  $SEINJECT --live "allow audioserver audioserver_tmpfs file { read write execute }"
-  $SEINJECT --live "allow audioserver system_file file execmod"
-  $SEINJECT --live "allow mediaserver mediaserver_tmpfs file { read write execute }"
-  $SEINJECT --live "allow mediaserver system_file file execmod"
+  $SEINJECT --live "allow audioserver audioserver_tmpfs file { read write execute }" "allow audioserver system_file file execmod" "allow mediaserver mediaserver_tmpfs file { read write execute }" "allow mediaserver system_file file execmod"
 else
   $SEINJECT -s audioserver -t audioserver_tmpfs -c file -p read,write,execute -l
   $SEINJECT -s audioserver -t system_file -c file -p execmod -l
